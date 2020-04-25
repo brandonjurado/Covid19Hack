a94950b7afa70ca3006e4c3a03c1e1090dff15c8
Michael Osei <mosei27@live.com>
2020-04-24 21:59:17 -0700

    Add complete render for jsx

--- public/javascripts/src/App.jsx
+++ public/javascripts/src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
+  console.log("Adding react component")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> Rendering with jsx now </div>
   );
 }
 
 export default App;
-
-const wrapper = document.getElementById("container");
-wrapper ? ReactDOM.render(<App />, wrapper) : false;

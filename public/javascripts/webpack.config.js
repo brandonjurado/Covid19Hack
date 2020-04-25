586ea2fe3dba5ac783cbdf431a794514deb7a39b
Michael Osei <mosei27@live.com>
2020-04-25 05:37:14 -0700

    Update frontend UI

--- public/javascripts/webpack.config.js
+++ public/javascripts/webpack.config.js
@@ -24,6 +24,20 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
       }
     ]
   },
